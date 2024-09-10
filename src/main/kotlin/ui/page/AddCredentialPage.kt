4f074f01f87137eafd4339453fb73cac1329687f
Христюк Игорь Анатольевич <IKhristyuk@alfabank.ru>
2024-09-10 09:03:09 +0300

    ui improvements

--- src/main/kotlin/ui/page/AddCredentialPage.kt
+++ src/main/kotlin/ui/page/AddCredentialPage.kt
@@ -12,7 +12,6 @@ import androidx.compose.material.ScaffoldState
 import androidx.compose.material.SnackbarDuration
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
